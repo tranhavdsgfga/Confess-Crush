d76013276a8a24c0053901d172286b8d515a10b7
tranhavdsgfga <94529366+tranhavdsgfga@users.noreply.github.com>
2021-11-17 18:43:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Thầy cô ',
+    introDesc: Chúc các thầy cô dồi dào sức khoẻ
+    , hạnh phúc để tiếp tục dìu dắt nhiều thế hệ học 
+    trò đến với những bến bờ tri thức.
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Học sinh 10a2 có ngoan không ạ 🥰',
+    desc: 'Chỉ chấp nhận câu trả lời thật lòng  ',
+    btnYes: 'siu ngoan <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Chúc thầy cô dồi dài sức khỏe',
+    btnReply: 'Gửi cho thầy cô <3',
+    reply: 'Thầy cô một ngày 20/11 vui vẻ<33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+  
+    messLink: https://www.youtube.com/watch?v=54yqn1BmrZs
 }
